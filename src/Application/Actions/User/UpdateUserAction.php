565b40b74bdde29f7c4731cb89cd7df5db8408f2
19ULmobDexep <s.zai@it-students.fr>
2020-03-13 13:35:15 +0100

    or

---
+++ src/Application/Actions/User/UpdateUserAction.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Application\Actions\User;
+
+use Psr\Http\Message\ResponseInterface as Response;
+
+class UpdateUserAction extends UserAction
+
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function action(): Response
+    {
+        $userId = (int) $this->resolveArg('id');
+
+        $datas= $this->getFormaData();
+
+        /**
+         * @var User
+         */
+        $user = $this->userRepository->findUserOfId($userId);
+
+        /**
+         * @var bool
+         */
+        $response = $user->update($datas);
+
+        $this->logger->info("User of id `${userId}` was viewed.");
+
+        return $this->respondWithData(['status'=>$response, "user"=>$user]);
+    }
+}
